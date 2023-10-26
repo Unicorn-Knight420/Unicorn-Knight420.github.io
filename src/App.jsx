264b682138232096b65572adbb9bd014ce3829e3
Unicorn-Knight420 <142096544+Unicorn-Knight420@users.noreply.github.com>
2023-10-26 13:24:50 +0200

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import React from 'react'
-import Navigation from './Components/Navigation/Navigation'
-import Accommodation from './Components/Accommodation/Accommodation'
-import Footer from './Components/Footer/Footer'
-import Search from './Components/Search/Search'
-import Value from './Components/Value/Value'
-import Preview from './Components/Preview/Preview'
-
 
 const App = () => {
-  return(
-    <div className='w-[85%] m-auto bg-white'>
-      <Navigation/>
-      <Search/>
-      <Preview/>
-      <Accommodation/>
-      <Value/>
-      <Footer/>
-    </div>
-  )
+    return(
+        <div>
+            
+        </div>
+    )
 }
 
 export default App
